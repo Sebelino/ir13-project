0804f55d662f55e12bd77080e1f62763994d58bf
RaxXo <oskar.bodemyr@gmail.com>
2013-05-09 12:30:40 +0200

    Search using the requesthandler called imageSearch

--- Search.py
+++ Search.py
@@ -6,12 +6,16 @@ class Search:
 	def __init__(self, solraddr):
 		self.solr = pysolr.Solr(solraddr, timeout=10)
 	def search(self, term):
-		return self.solr.search(term, **{
+		'''
+		Search using the requestHandler named imageSearch.
+		If the requestHandler works, so should this query
+		'''
+		return self.solr.search(term,**{
 			'qt':'imageSearch',
 			})
 
 if __name__ == '__main__': 
-	s = Search('http://localhost:8080/solr/test3')
+	s = Search('http://localhost:8080/solr/test3/')
 	input = ""
 	while(input != "exit"):
 		input = raw_input("What would you like to search for? (exit-command: exit)\n>")
