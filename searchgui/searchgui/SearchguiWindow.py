b4a5bdb35d26b7bd599b4dbb612e6c06078c0e1a
RaxXo <oskar.bodemyr@gmail.com>
2013-05-16 23:24:19 +0200

    more GUI stuff

--- searchgui/searchgui/SearchguiWindow.py
+++ searchgui/searchgui/SearchguiWindow.py
@@ -5,8 +5,14 @@
 
 from locale import gettext as _
 
+import pygtk
+pygtk.require('2.0')
 from gi.repository import Gtk # pylint: disable=E0611
+from gi.repository.GdkPixbuf import Pixbuf
 import logging
+import urllib2
+import gobject
+
 logger = logging.getLogger('searchgui')
 
 from searchgui_lib import Window
@@ -28,9 +34,29 @@ class SearchguiWindow(Window):
         # Code for other initialization actions should be added here.
     def perform_search(self,sender):
         s = Search()
-        grid = self.ui.image_result_grid
+        view = self.ui.image_result_grid
         query = self.ui.query_input.get_text()
         results = s.search(query)
+        model = Gtk.ListStore(Pixbuf,  gobject.TYPE_STRING)
+        view = Gtk.IconView(model)  # Pass the model stored in a ListStore to the GtkIconView
+        view.set_pixbuf_column(0)
+        view.set_text_column(1)
+        view.set_selection_mode(Gtk.SELECTION_MULTIPLE)
+        view.set_columns(0)
+        view.set_item_width(150)
         for result in results:
+
             print result
+
+    def get_image_from_url(self, url):
+        response = urllib2.urlopen(url)
+        fname = url.split("/")[-1]
+        f = open(fname, "wb")
+        f.write(response.read())
+        f.close()
+        response.close()
+        image = Gtk.Image()
+        image.set_from_pixbuf(Pixbuf.new_from_file(fname))
+        return image
+
 
