f8d5e4c1d060e84491b50101e5342b05f66a0827
tryti <tryti@kth.se>
2013-05-16 11:52:28 +0200

    Work in progress

--- jo-crawler/Crawler.py
+++ jo-crawler/Crawler.py
@@ -1,5 +1,4 @@
 from Scraper import Scraper
-from Scraper import WikipediaScraper
 from mechanize import Browser
 import ImageDocument
 import random
@@ -10,50 +9,63 @@ __author__ = 'Tryti'
 WIKIPEDIA_EN_RANDOM = 'en.wikipedia.org/wiki/Special:Random'
 MAXSCRAPES = 10
 
-
 class Crawler:
     def __init__(self, headurl, maxnr=MAXSCRAPES):
-        '''
+        """
         Starts to crawl from the headurl and continues at random
-        '''
+        """
+        self.visited = []
+        self.maxnr = maxnr
+        self.next_link = headurl
 
+    def crawl(self):
         #This is the browser we use
         self.br = Browser()
 
-        self.imageDocuments = []
         scraped = 0
-        self.links = [headurl]
-        self.visited = []
-
         #Scrape untill we scraped wanted amount or we fall into a sink
-        while (scraped < maxnr) and (len(self.links) != 0):
-            #Start scraping the site
-            temp_url = self.links.pop(random.randint(0, len(self.links) - 1))
-            if not (temp_url in self.visited):
-                self.visited.append(temp_url)
-                self.scrape(temp_url)
+        while (scraped < self.maxnr) and (self.next_link):
+            #Get page contents with Browser
+            self.br.open(self.next_link)
+            #And add it to the visited list
+            self.visited.append(self.next_link)
 
-                scraped += 1
+            #if its html then we wants to scrape it
+            if self.br.viewing_html() :
+                #Get the imageDocuments on the site
+                s = Scraper(self.br.response().read(), self.next_link)
+                image_documents = s.get_image_documents()
 
-        self.br.close()
-
-    def scrape(self, url):
-        #Get page contents with Browser
-        self.br.open(url)
-        if self.br.viewing_html():
-            #Get the imageDocuments on the site
-            scraper = Scraper(self.br.response().read(), url)
-            newImages = scraper.get_image_documents()
-            self.imageDocuments += newImages
-            #Get the links from the site and add them to the
+                for i in image_documents:
+                    yield i
+                scraped+=1
+            
+            next_links = []
+            #Get the links from the site and add them to the 
             for i in self.br.links(url_regex='http'):
                 if (i.url != '#page'):
-                    self.links.append(i.url)
+                    next_links.append(i.url)
+
+            self.next_link = None
+
+            #Unless we are at a sink we wants to continue
+            if(len(next_links) != 0):
+                while not self.next_link:
+                    temp_link = self.random_link(next_links)
+                    if (temp_link in self.visited):
+                        self.next_link = None
+                    else:
+                        self.next_link = temp_link
+    
+        self.br.close() 
+
+    def random_link(self, links):
+        return links.pop(random.randint(0, len(links)-1))
+
 
 
 def main():
     test()
-
 
 def test():
     url = 'http://www.xkcd.com'
@@ -63,7 +75,7 @@ def test():
 
     print('')
     print('Found these images')
-    for i in c.imageDocuments:
+    for i in c.crawl():
         print(i.url)
 
 if __name__ == '__main__':
