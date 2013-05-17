4d4309d86be90e7b6a6e96542fc37d9f1850f945
Daan Wynen <black.puppydog@gmx.de>
2013-05-17 03:53:29 +0200

    pimp tha hell outta clippy.

--- imageflask/imageflask.py
+++ imageflask/imageflask.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request
 import GlobalConfiguration
 from Search import Search
+import random
 
 app = Flask(__name__)
 
+clippyjs_agents = ['Bonzi', 'Clippy', 'F1', 'Genie', 'Genius', 'Links', 'Merlin', 'Peedy', 'Rocky', 'Rover']
+
+
+def random_clippyjs_agent():
+    return clippyjs_agents[random.randint(0, len(clippyjs_agents)-1)]
 
 @app.route('/')
 def home():
-    return render_template('search.html')
+    return render_template('search.html', clippyjs_agent=random_clippyjs_agent())
 
 @app.route('/s', methods=['POST'])
 def search():
@@ -15,7 +21,7 @@ def search():
     s = Search()
     results = s.search(query)
 
-    return render_template('search.html', query=query, results=results)
+    return render_template('search.html', query=query, results=results, clippyjs_agent=random_clippyjs_agent())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
