d1bffd56d60d6e61eecbeca21d5d13965745e9b5
Daan Wynen <black.puppydog@gmx.de>
2013-05-17 03:26:47 +0200

    we rock so hard.

--- imageflask/imageflask.py
+++ imageflask/imageflask.py
@@ -7,16 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-        return render_template('search.html')
+    return render_template('search.html')
 
 @app.route('/s', methods=['POST'])
 def search():
     query = request.form['query']
     s = Search()
     results = s.search(query)
-    response = 'Query: %s \n' % query
-    response += '\n'.join(results)
-    return response
+
+    return render_template('search.html', query=query, results=results)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
